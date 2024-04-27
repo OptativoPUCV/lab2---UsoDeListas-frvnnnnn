45f90a5bf1e2b518ff21543ad7320da2829dd165
Your Name <you@example.com>
2024-04-27 04:25:46 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -147,9 +147,9 @@ int parentesisBalanceados(char *cadena) {
     }
     
     if (P1!=NULL)
-      return 1;  // Paréntesis balanceados
+      return 0;  // Paréntesis balanceados
     else
-      return 0;  // Paréntesis no balanceados
+      return 1;  // Paréntesis no balanceados
 }
 
 
